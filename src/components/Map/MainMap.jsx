6b8bbb9880596daedc9d6d291469e3b98ac14026
Harsh <harshsabhaya10@gmail.com>
2024-03-15 17:21:44 -0400

    Feat: Brussel map integrated in home page

---
+++ src/components/Map/MainMap.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+const position = [50.8476, 4.3572];
+const icon = L.icon({
+  iconUrl: './placeholder.png',
+  iconSize: [38, 38],
+});
+
+const mapType = [
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
+];
+
+const ResetCenterView = (props) => {
+  const { selectedPosition } = props;
+  const map = useMap();
+
+  useEffect(() => {
+    if (selectedPosition) {
+      map.setView(
+        L.latLng(selectedPosition?.lat, selectedPosition?.lon),
+        map.getZoom(),
+        {
+          animate: true,
+        }
+      );
+    }
+  }, [map, selectedPosition]);
+
+  return null;
+};
+
+const Maps = (props) => {
+  const { selectedPosition } = props;
+  const markerPosition = [
+    selectedPosition?.lat ?? position[0],
+    selectedPosition?.lon ?? position[1],
+  ];
+  return (
+    <div style={{ width: '100%', height: 'calc(100vh - 111px)' }}>
+      <MapContainer
+        center={position}
+        zoom={12}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url={mapType[0]} />
+        <Marker position={markerPosition} icon={icon}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <ResetCenterView selectedPosition={selectedPosition} />
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Maps;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-
+import React from 'react';
+import HeatMap from './HeatMap';
+import MainMap from '../components/Map/MainMap';
 
 const Home = () => {
   const location = {
     lat: 37.7749, // Latitude of the location
-    lng: -122.4194 // Longitude of the location
+    lng: -122.4194, // Longitude of the location
   };
 
   const zoomLevel = 12; // Zoom level of the map
 
   return (
-    <div className="app">
-      <h1>Hellolahsdkfghlksdhgfklh</h1>
+    <div className='app'>
+      <MainMap />
+      {/* <HeatMap /> */}
     </div>
   );
-}
+};
 
 export default Home;
