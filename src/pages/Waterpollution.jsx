1dfb84590b7e5df088641829807d501f03d7b227
Harsh <harshsabhaya10@gmail.com>
2024-04-06 17:35:46 -0400

    Fix: Waterpollution file import changed

--- src/pages/Waterpollution.jsx
+++ src/pages/Waterpollution.jsx
@@ -3,28 +3,28 @@ import { Typography, Box } from '@mui/material';
 
 /**
  * WaterPollution.jsx
- * 
+ *
  * This is a simple React component that displays a "Work in Progress" message.
- * 
+ *
  * Dependencies:
  * - React
  * - @mui/material
- * 
+ *
  * Functions:
  * - None
- * 
+ *
  * State Variables:
  * - None
- * 
+ *
  * Component Return:
  * - The component returns a div element that contains a Box component and Typography components.
  * - The Typography components display a "Work in Progress" message and a description.
- * 
+ *
  * Example Usage:
  * ```jsx
  * <WaterPollution />
  * ```
- * 
+ *
  * Note:
  * - This component is currently under development.
  */
